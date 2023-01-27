1dca5b136be070b4220ac8a32e47fdf36fc80295
okatu-loli <CN_QianShi@hotmail.com>
2023-01-28 01:25:40 +0800

    增加错误处理

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def handle_github_webhook():
     # 获取github webhook发送的json数据
     github_json = request.get_json()
-    repo = github_json["repository"]["name"]
-    commit_msg = github_json["head_commit"]["message"]
-    commit_url = github_json["head_commit"]
-    repo_url = github_json["repository"]["html_url"]
+    repo = github_json.get("repository", {}).get("name", "未获取到仓库名")
+    commit_msg = github_json.get("head_commit", {}).get("message", "未获取到commit的内容，可能为非commit操作，点击下方的commit地址可在浏览器查看具体错误")
+    commit_url = github_json.get("head_commit", {}).get("url", "未获取到commit的链接，可能为非commit操作")
+    repo_url = github_json.get("repository", {}).get("html_url", "未获取到仓库链接")
     # 解析github webhook发送的json数据，生成飞书API支持的json数据
     feishu_json = {
         "msg_type": "post",
@@ -27,8 +27,13 @@ def handle_github_webhook():
                         },
                             {
                                 "tag": "a",
-                                "text": f"请点击查看仓库",
+                                "text": f"请点击查看仓库\n",
                                 "href": repo_url
+                            },
+                            {
+                                "tag": "a",
+                                "text": f"请点击查看commit地址\n",
+                                "href": commit_url
                             }
                         ]
                     ]
@@ -41,6 +46,7 @@ def handle_github_webhook():
     headers = {'Content-Type': 'application/json'}
     # 将飞书API支持的json数据转换为字符串并发送给飞书机器人
     jsons = json.dumps(feishu_json)
+    print(jsons)
     response = requests.post(url, jsons, headers=headers)
     if response.status_code != 200:
         print(response.text)
